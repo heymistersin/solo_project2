7ce1a7a5ae6e39b4962cd09fbb35c1870c862958
heymistersin <99973059+heymistersin@users.noreply.github.com>
2024-01-08 11:14:51 -0500

    forms are up but no submitting

--- src/components/SetForm.jsx
+++ src/components/SetForm.jsx
@@ -1,15 +1,58 @@
 import React from "react";
+import { useState } from "react";
 
 const Set = () => {
+
+  const [newSet, setNewSet] = useState("");
+  // const [input, setInput] = useState({ exerciseName: '', weight: '', reps: ''});
+  const [input, setInput] = useState([]);
+  // console.log(input);
+
+  const handleSubmit = (event) => {
+    event.preventDafault(); // prevent page from refreshing
+
+    setInput((input) => {
+      return [
+        ...input.value
+      ]
+    })
+
+    setNewSet("")
+  }
+  
+  // const change = (e) => {
+  //   setNewSet(e.target.value)
+  // }
+
   return (
     <div>
-      <form>
-        <input type="text" name="excersize_name" id="excersize_name" placeholder="excersize name" />
+      {/* <form> */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="excersize_name" 
+          id="excersize_name" 
+          placeholder="excersize name"
+          value={newSet}
+          onChange={e => setNewSet(e.target.value)}
+        />
+
         <input type="number" name="weight" id="weight" placeholder="lbs" />
         <input type="number" name="reps" id="reps" placeholder="Must be greater than 0" />
 
-        <button>submit</button>
+        <button type="submit">Submit</button>
       </form>
+
+      <ol>
+        {input.map(e => {
+          return(
+            <li key={input.id}>
+              {input.value}
+            </li>
+          )
+        })}
+        {/* <li>{newSet}</li> */}
+      </ol>
     </div>
   )
 }
